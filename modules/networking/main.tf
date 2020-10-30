a8f33d2a43a45fdc794c53836671bb9c2c3283fc
Alex <alex@alxit.net>
2020-10-29 20:37:10 -0400

    egress on the load balancer is a good idea...

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -74,6 +74,13 @@ resource "aws_security_group" "lb_sg" {
         protocol = "tcp"
         cidr_blocks = ["${var.ssh_public_ip}"]
     }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 module "alb" {
