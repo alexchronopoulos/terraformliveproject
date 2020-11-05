fc055659c40dbf3a231aafd1729cb15abb0594e3
Alex <alex@alxit.net>
2020-11-04 20:52:35 -0500

    try adding table with just an ID attribute

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,26 +1,10 @@
 resource "aws_dynamodb_table" "dynamodb-table" {
   name           = "${var.namespace}-table"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "Category"
+  hash_key       = "ID"
 
   attribute {
-    name = "Category"
+    name = "ID"
     type = "S"
   }
-
-  attribute {
-    name = "Beverage"
-    type = "S"
-  }
-
-  attribute {
-    name = "Size"
-    type = "S"
-  }
-
-  attribute {
-    name = "Price"
-    type = "S"
-  }
-  
 }
